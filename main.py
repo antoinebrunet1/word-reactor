d06532767c65be18aa43db2826338caad6352522
Antoine Brunet <antoinebrunet1@outlook.com>
2026-03-30 19:41:10 -0400

    Made on_message print message replied to in case of reply

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ class Client(discord.Client):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        # If the message is a reply
+        if message.reference and message.reference.message_id:
+            replied = await message.channel.fetch_message(message.reference.message_id)
+            print(replied.content)
 
 intents = discord.Intents.default()
 intents.message_content = True
