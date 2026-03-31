79b0196c380d789497951046db084d4bd10b28c4
Antoine Brunet <antoinebrunet1@outlook.com>
2026-03-31 03:17:56 -0400

    Turned is_message_reply into a function

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ load_dotenv()
 
 token=os.getenv("TOKEN")
 
+
+async def is_message_reply(message):
+    return message.reference and message.reference.message_id
+
+
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        if not self.is_message_reply(message):
+        if not is_message_reply(message):
             return
         
         replied = await message.channel.fetch_message(message.reference.message_id)
         print(replied.content)
 
-    async def is_message_reply(self, message):
-        return message.reference and message.reference.message_id
 
 intents = discord.Intents.default()
 intents.message_content = True
