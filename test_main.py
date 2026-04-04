483c76ddfaa388d8b3b41c2592e227447b46974a
Antoine Brunet <antoinebrunet1@outlook.com>
2026-04-04 03:03:11 -0400

    Added test_react_invalid_word

--- test_main.py
+++ test_main.py
@@ -1,4 +1,8 @@
-from main import is_word_valid, word_to_letters_emojis_array, letter_to_letter_emoji
+from unittest.mock import AsyncMock
+
+import pytest
+
+from main import is_word_valid, word_to_letters_emojis_array, letter_to_letter_emoji, react
 
 
 def test_is_word_valid_happy_path():
@@ -16,3 +20,17 @@ def test_word_to_letters_emojis_array_happy_path():
 
 def test_letter_to_letter_emoji_happy_path():
     assert letter_to_letter_emoji("H") == "🇭"
+
+@pytest.mark.asyncio
+async def test_react_invalid_word(mocker):
+    mock_interaction = AsyncMock()
+    message_id = "0"
+    word = "hhi"
+    mock_is_word_valid = mocker.patch("main.is_word_valid")
+    mock_is_word_valid.return_value = False
+
+    await react.callback(mock_interaction, message_id, word)
+
+    expected_error_message = "Error: The letters should be in the union of A-Z and a-z and no letters should repeat."
+
+    mock_interaction.response.send_message.assert_awaited_once_with(expected_error_message)
