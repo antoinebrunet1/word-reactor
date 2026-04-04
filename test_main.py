eed9c5d5ec81a12f9f28fe79a04a6eeaa2e54b0e
Antoine Brunet <antoinebrunet1@outlook.com>
2026-04-04 03:43:35 -0400

    Added test_react_happy_path

--- test_main.py
+++ test_main.py
@@ -34,3 +34,17 @@ async def test_react_invalid_word(mocker):
     expected_error_message = "Error: The letters should be in the union of A-Z and a-z and no letters should repeat."
 
     mock_interaction.response.send_message.assert_awaited_once_with(expected_error_message)
+
+
+@pytest.mark.asyncio
+async def test_react_happy_path(mocker):
+    mock_interaction = AsyncMock()
+    mocker.patch("main.is_word_valid").return_value = True
+    mock_message_to_react_to = AsyncMock()
+    mock_interaction.channel.fetch_message.return_value = mock_message_to_react_to
+    mocker.patch("main.word_to_letters_emojis_array").return_value = ["🇭"]
+
+    await react.callback(mock_interaction, "0", "h")
+
+    mock_message_to_react_to.add_reaction.assert_awaited_once_with("🇭")
+    mock_interaction.response.send_message.assert_awaited_once_with("Successfully reacted.")
