edd7372cf8cc79de7b798f93b074413ca54fb0b8
Antoine Brunet <antoinebrunet1@outlook.com>
2026-04-03 20:41:27 -0400

    Added test_word_to_letters_emojis_array_happy_path

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
-from main import is_word_valid
+from main import is_word_valid, word_to_letters_emojis_array
+
 
 def test_is_word_valid_happy_path():
     assert is_word_valid("hi")
@@ -8,3 +9,7 @@ def test_is_word_valid_repeating_letters():
 
 def test_is_word_valid_non_alpha_letters():
     assert not is_word_valid("hi2")
+
+def test_word_to_letters_emojis_array_happy_path():
+    actual_array = word_to_letters_emojis_array("HI")
+    assert ' '.join(actual_array) == "🇭 🇮"
