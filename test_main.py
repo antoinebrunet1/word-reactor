833a5195604790384b80290604ae0d100b25acf7
Antoine Brunet <antoinebrunet1@outlook.com>
2026-04-03 20:15:53 -0400

    Added test_is_word_valid_repeating_letters

--- test_main.py
+++ test_main.py
@@ -2,3 +2,6 @@ from main import is_word_valid
 
 def test_is_word_valid_happy_path():
     assert is_word_valid("hi")
+
+def test_is_word_valid_repeating_letters():
+    assert not is_word_valid("hhi")
